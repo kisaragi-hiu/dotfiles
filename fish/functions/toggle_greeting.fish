befab2d98f7209bbbab32fbd9a8e849c6334f98e
Moonlight Feather <flyingfeather1501@gmail.com>
2017-02-08 04:01:16 +0800

    Remove echoes in the lines

--- fish/functions/toggle_greeting.fish
+++ fish/functions/toggle_greeting.fish
@@ -1,13 +1,10 @@
 function toggle_greeting
     if test $fish_disable_greeting -eq 1 2> /dev/null
         set -gx fish_disable_greeting 0
-        echo 0
     else if test $fish_disable_greeting -eq 0 2> /dev/null
         set -gx fish_disable_greeting 1
-        echo 1
     else if test -z $fish_disable_greeting # if its length is zero
         set -gx fish_disable_greeting 1
-        echo 2
     else
         # fish_disable_greeting is something else
         # complain
