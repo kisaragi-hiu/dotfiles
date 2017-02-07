d254b0804ad05ba09d382a92b848924b42557371
Moonlight Feather <flyingfeather1501@gmail.com>
2017-02-08 03:38:15 +0800

    Not using gnome-keyring for chrome anymore, no need for the patching sed wrapper

--- fish/functions/patch-chrome-passwdstore.fish
+++
@@ -1,5 +0,0 @@
-function patch-chrome-passwdstore
-  for i in ~/.local/share/applications/chrome-*
-    sed -i 's/google-chrome-unstable/google-chrome-unstable --password-store=gnome/g' $i
-  end
-end
