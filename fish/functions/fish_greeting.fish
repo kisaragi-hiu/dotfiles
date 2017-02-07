3551613cf03bb46e3d522defb514f9bb3f4a037b
Moonlight Feather <flyingfeather1501@gmail.com>
2017-02-08 04:00:36 +0800

    don't know how to use or/-o, only testing fish_disable_greeting for 0 now

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,7 +1,5 @@
 function fish_greeting
-    if test $fish_disable_greeting -eq 1 -o\
-            $fish_disable_greeting -eq "true" -o\
-            $fish_disable_greeting -eq "yes" 2> /dev/null
+    if test $fish_disable_greeting -eq 1 2> /dev/null
     # do nothing
     else
         Sudocabulary
