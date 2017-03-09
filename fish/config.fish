feb0c4f5303fc8b0c172482b19304bc33da0af91
Moonlight Feather <flyingfeather1501@gmail.com>
2017-03-10 02:37:26 +0800

    disable powerline for now

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,3 @@
-powerline-daemon -q
-set fish_function_path $fish_function_path "/usr/lib/python3.6/site-packages/powerline/bindings/fish"
-powerline-setup
+#powerline-daemon -q
+#set fish_function_path $fish_function_path "/usr/lib/python3.6/site-packages/powerline/bindings/fish"
+#powerline-setup
