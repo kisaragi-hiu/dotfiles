508af9e527a925f60176e5176b518be594124377
Moonlight Feather <flyingfeather1501@gmail.com>
2017-04-29 23:43:02 +0800

    Move some autostarts from autostart/ to autostart-scripts/autostart.sh

--- autostart-scripts/autostart.sh
+++ autostart-scripts/autostart.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 # Put all autostart commands in one file
 # so I don't need to rely on links
+
+##-------REMEMBER TO PUT THEM IN BACKGROUND-------##
+echo $(date): priority test from ~/.config/autostart-scripts/autostart.sh >> $HOME/.priority_test &
+
+# Vocabulary notification
+loop-command 30m 0 pop-random-notification\
+  /home/flyin1501/git/notification-srs/vocab-files/vocab-word-30m.txt\
+  /home/flyin1501/git/notification-srs/vocab-files/vocab-meaning-30m.txt &
+
+# Grive
+loop-command 10m 0 grive -p $G/ &
+
+ssh-agent &
+
+numlockx &
 screenstalk -t 10 &
-plank &
-tilda &
+sleep 4; tilda &
