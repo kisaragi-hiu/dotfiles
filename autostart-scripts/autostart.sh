738c172e02871b2118c566f84c038d78e91fd1ac
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-05-22 17:31:47 +0800

    Restructure some comments; megasync launched after tilda

--- autostart-scripts/autostart.sh
+++ autostart-scripts/autostart.sh
@@ -3,18 +3,20 @@
 # so I don't need to rely on links
 
 ##-------REMEMBER TO PUT THEM IN BACKGROUND-------##
-echo $(date): priority test from ~/.config/autostart-scripts/autostart.sh >> $HOME/.priority_test &
+echo $(date): does $G work? >> $HOME/.priority_test &
 
 # Vocabulary notification
 loop-command 30m 0 pop-random-notification\
   /home/flyin1501/git/notification-srs/vocab-files/vocab-word-30m.txt\
   /home/flyin1501/git/notification-srs/vocab-files/vocab-meaning-30m.txt &
 
-# Grive
-loop-command 10m 0 grive -p $G/ &
-
+loop-command 10m 0 grive -p /run/media/flyin1501/Data/Google\ ドライブ/ & # Grive
 ssh-agent &
-
 numlockx &
 screenstalk -t 10 &
+
+# the sleep isn't put in the background, have to find another place for them probably
 sleep 4; tilda &
+
+# Megasync would put a window right at the upper-left corner if started before the tray
+sleep 2m; megasync &
