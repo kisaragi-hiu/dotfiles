4632b550aaf14c2c32b602ebe43e44cd61bbe0c5
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-06-12 00:17:21 +0800

    New paths for vocab files

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -2,7 +2,7 @@ function fish_greeting
     if test $fish_disable_greeting -eq 1 2> /dev/null
     # do nothing
     else
-        Sudocabulary
+        Sudocabulary ~/git/text-files/vocab-{word,meaning}-30m.txt
         echo # print extra line
     end
 end
