662ccd46d957ef8e524123e67b7141c0382ff76d
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-06-12 00:16:51 +0800

    New paths for vocab files; new grive path

--- autostart-scripts/autostart.sh
+++ autostart-scripts/autostart.sh
@@ -2,21 +2,15 @@
 # Put all autostart commands in one file
 # so I don't need to rely on links
 
-##-------REMEMBER TO PUT THEM IN BACKGROUND-------##
-echo $(date): does $G work? >> $HOME/.priority_test &
-
 # Vocabulary notification
 loop-command 30m 0 pop-random-notification\
-  /home/flyin1501/git/notification-srs/vocab-files/vocab-word-30m.txt\
-  /home/flyin1501/git/notification-srs/vocab-files/vocab-meaning-30m.txt &
+  /home/flyin1501/git/text-files/vocab-word-30m.txt\
+  /home/flyin1501/git/text-files/vocab-meaning-30m.txt &
 
-loop-command 10m 0 grive -p /run/media/flyin1501/Data/Google\ ドライブ/ & # Grive
+loop-command 10m 0 grive -p /home/flyin1501/ドキュメント/
 ssh-agent &
 numlockx &
 screenstalk -t 10m &
-
-# the sleep isn't put in the background, have to find another place for them probably
-sleep 4; tilda &
 
 # Megasync would put a window right at the upper-left corner if started before the tray
 sleep 2m; megasync &
