07564ed4bc36a6d2f889d7d984c5f1bc4d38d633
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-07-28 22:58:32 +0800

    Start terminology if in a tty

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
 #powerline-daemon -q
 #set fish_function_path $fish_function_path "/usr/lib/python3.6/site-packages/powerline/bindings/fish"
 #powerline-setup
+if test (loginctl show-session $XDG_SESSION_ID -p Type) = "Type=tty"
+  terminology
+end
