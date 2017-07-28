4b731259627b94a70bf6aec5229d2738cd2423f1
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-07-28 23:12:58 +0800

    A snippet should be in conf.d

--- fish/conf.d/terminology.fish
+++ fish/conf.d/terminology.fish
@@ -1,7 +1,3 @@
-#powerline-daemon -q
-#set fish_function_path $fish_function_path "/usr/lib/python3.6/site-packages/powerline/bindings/fish"
-#powerline-setup
-
 test (loginctl show-session $XDG_SESSION_ID -p Type) = "Type=tty" # are we in a tty?
 and which terminology >/dev/null 2>/dev/null # is terminology installed?
 and terminology >/dev/null 2>/dev/null # start terminology
