04db1f293ae0998d741fe9e72f4cf4630bd1b370
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-07-28 23:08:16 +0800

    Improve terminology starting snippet

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,8 @@
 #powerline-daemon -q
 #set fish_function_path $fish_function_path "/usr/lib/python3.6/site-packages/powerline/bindings/fish"
 #powerline-setup
-if test (loginctl show-session $XDG_SESSION_ID -p Type) = "Type=tty"
-  terminology
-end
+
+test (loginctl show-session $XDG_SESSION_ID -p Type) = "Type=tty" # are we in a tty?
+and which terminology >/dev/null 2>/dev/null # is terminology installed?
+and terminology >/dev/null 2>/dev/null # start terminology
+or true # handle non-zero exit code
