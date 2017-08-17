1fe3c7f0e4e115b5fda37fa240e4c7583363c9c8
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-08-17 21:18:53 +0800

    Check parent isn't terminology before starting terminology in a tty

--- fish/conf.d/terminology.fish
+++ fish/conf.d/terminology.fish
@@ -1,4 +1,18 @@
-test (loginctl show-session $XDG_SESSION_ID -p Type) = "Type=tty" # are we in a tty?
-and which terminology >/dev/null 2>/dev/null # is terminology installed?
-and terminology >/dev/null 2>/dev/null # start terminology
-or true # handle non-zero exit code
+function get_parent
+  echo (ps -o \
+         comm= (cat (set self %self
+                     echo /proc/$self/status) \
+                | grep PPid \
+                | cut -d\t -f 2))
+  set -e self
+end
+
+if test (loginctl show-session $XDG_SESSION_ID -p Type) = "Type=tty" # are we in a tty?
+if which terminology >/dev/null 2>/dev/null # is terminology installed?
+if test (get_parent) != "terminology" # is fish not running under terminology?
+
+terminology >/dev/null 2>/dev/null # start terminology when all above are true
+
+end
+end
+end
