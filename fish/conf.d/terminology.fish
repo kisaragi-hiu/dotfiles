6a2c7f900566829f6b7ac3c155a0d3bbb39340dd
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-08-22 18:45:07 +0800

    test parent process = login instead of check != terminology

--- fish/conf.d/terminology.fish
+++ fish/conf.d/terminology.fish
@@ -9,7 +9,7 @@ end
 
 if test (loginctl show-session $XDG_SESSION_ID -p Type) = "Type=tty" # are we in a tty?
 if which terminology >/dev/null 2>/dev/null # is terminology installed?
-if test (get_parent) != "terminology" # is fish not running under terminology?
+if test (get_parent) = "login" # is fish evoked by login, ie. directly in a tty?
 
 terminology >/dev/null 2>/dev/null # start terminology when all above are true
 
