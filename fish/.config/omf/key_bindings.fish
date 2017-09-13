a822e568c09502d69ada12b6e49fb8e2b3eedc83
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-09-13 23:55:48 +0800

    Bind C-j to `jobs`

--- fish/.config/omf/key_bindings.fish
+++ fish/.config/omf/key_bindings.fish
@@ -1,2 +1,3 @@
 bind \cg 'toggle_greeting; and echo \nToggled greeting message.; commandline -f repaint'
 bind \cl "commandline -C 0; commandline -i 'launch '"
+bind \cj 'jobs; commandline -f repaint'
