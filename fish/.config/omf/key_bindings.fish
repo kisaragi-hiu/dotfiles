241f57f5d0ec80687555783f364caab23cafe32e
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-10-07 19:51:24 +0800

    Fish: M-g -> git status

--- fish/.config/omf/key_bindings.fish
+++ fish/.config/omf/key_bindings.fish
@@ -1,3 +1,4 @@
 bind \cg 'toggle_greeting; and echo \nToggled greeting message.; commandline -f repaint'
 bind \cl "commandline -C 0; commandline -i 'launch '"
 bind \ej 'echo; jobs; commandline -f repaint'
+bind \eg 'echo; git status; commandline -f repaint'
