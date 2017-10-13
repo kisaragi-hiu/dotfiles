9194b827cdf08e5d87f9316d05a9c84846382f80
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-10-13 18:24:13 +0800

    Add git aliases (gc, gd, g↓, g↑)

--- fish/.config/fish/conf.d/alias.fish
+++ fish/.config/fish/conf.d/alias.fish
@@ -5,7 +5,12 @@ alias screenshot 'import -window root'
 
 alias clear 'clear; fish_greeting'
 alias diff 'diff --color=auto'
+
 alias git 'hub'
+alias g↑ 'git push'
+alias g↓ 'git pull'
+alias gc 'git commit'
+alias gd 'git diff'
 
 alias pacman-list-orphans 'pacman -Qtd'
 alias pacman-remove-orphans 'sudo pacman -Rns (pacman -Qtdq)'
