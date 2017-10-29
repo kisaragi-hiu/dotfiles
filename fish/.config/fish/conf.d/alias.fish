68d8f637939a5b7ec4ad7801f44019ae6cbad0b0
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-10-29 15:34:28 +0800

    Alias ls -> exa, alias gco -> git checkout

--- fish/.config/fish/conf.d/alias.fish
+++ fish/.config/fish/conf.d/alias.fish
@@ -11,8 +11,11 @@ alias g↑ 'git push'
 alias g↓ 'git pull'
 alias gc 'git commit'
 alias gd 'git diff'
+alias gco 'git checkout'
 
 alias pacman-list-orphans 'pacman -Qtd'
 alias pacman-remove-orphans 'sudo pacman -Rns (pacman -Qtdq)'
 
 alias vim 'echo using nvim...; sleep 0.1; nvim'
+
+alias ls 'echo Using exa.; echo; exa'
