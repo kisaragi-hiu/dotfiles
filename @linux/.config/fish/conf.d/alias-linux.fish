e2b55e53602d38df726786b3aee75c00b96f8233
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-11-06 18:28:26 +0800

    Fish: Move device specific autostart functions to @linux

---
+++ @linux/.config/fish/conf.d/alias-linux.fish
@@ -0,0 +1,9 @@
+alias frog 'raco frog'
+alias pollen 'raco pollen'
+
+alias screenshot 'import -window root'
+
+alias pacman-list-orphans 'pacman -Qtd'
+alias pacman-remove-orphans 'sudo pacman -Rns (pacman -Qtdq)'
+
+alias ls 'echo Using exa.; echo; exa'
--- fish/.config/fish/conf.d/alias.fish
+++ fish/.config/fish/conf.d/alias.fish
@@ -1,8 +1,3 @@
-alias frog 'raco frog'
-alias pollen 'raco pollen'
-
-alias screenshot 'import -window root'
-
 alias clear 'clear; fish_greeting'
 alias diff 'diff --color=auto'
 
@@ -13,9 +8,4 @@ alias gc 'git commit'
 alias gd 'git diff'
 alias gco 'git checkout'
 
-alias pacman-list-orphans 'pacman -Qtd'
-alias pacman-remove-orphans 'sudo pacman -Rns (pacman -Qtdq)'
-
 alias vim 'echo using nvim...; sleep 0.1; nvim'
-
-alias ls 'echo Using exa.; echo; exa'
