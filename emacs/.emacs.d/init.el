5817c47fd28d3fa483e5a2fc1d220128d4024a04
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-11-13 21:16:49 +0800

    Emacs: Indent with spaces by default & keep current file's indentation

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -19,6 +19,18 @@
 (delete-selection-mode 1)
 (setq tab-always-indent 'complete)
 
+;; Good'ol EmacsWiki
+;; Indent with spaces by default
+(defun infer-indentation-style ()
+  ;; if our source file uses tabs, we use tabs, if spaces spaces, and if
+  ;; neither, we use the current indent-tabs-mode
+  (let ((space-count (how-many "^  " (point-min) (point-max)))
+        (tab-count (how-many "^\t" (point-min) (point-max))))
+    (if (> space-count tab-count) (setq indent-tabs-mode nil))
+    (if (> tab-count space-count) (setq indent-tabs-mode t))))
+(setq indent-tabs-mode nil)
+(infer-indentation-style)
+
 (use-package evil
   :init
   ;; evil-leader should be enabled before evil loads
