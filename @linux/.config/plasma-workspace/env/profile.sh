d00c581f6edd74c98a1e9aefc04154de661cefba
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-11-23 20:06:13 +0900

    .profile: use $HOME variable (and surpress ShellCheck's source warnings)

--- @linux/.config/plasma-workspace/env/profile.sh
+++ @linux/.config/plasma-workspace/env/profile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC1090
 
 # use qt5ct outside plasma
 if [ "$XDG_CURRENT_DESKTOP" != plasma ] || [ "$XDG_CURRENT_DESKTOP" != KDE ]; then
@@ -10,7 +11,7 @@ fi
 export ELECTRON_FORCE_WINDOW_MENU_BAR=1
 
 # setup GOPATH
-export GOPATH=/home/flyin1501/.gopath
+export GOPATH="$HOME"/.gopath
 
 # for storing ssh keys with kdewallet
 export SSH_ASKPASS="/usr/bin/ksshaskpass"
@@ -40,7 +41,7 @@ export QT_IM_MODULE=fcitx
 export XMODIFIERS=@im=fcitx
 
 # filmic Blender can be loaded this way
-export OCIO=/home/flyin1501/.dotfiles/_filmic-blender/config.ocio
+export OCIO="$HOME"/.dotfiles/_filmic-blender/config.ocio
 
 # turn off wine debugging
 export WINEDEBUG=-all
@@ -52,20 +53,20 @@ export EDITOR=nvim
 # dir variables
 export D=/run/media/flyin1501/Data
 export C=/run/media/flyin1501/Windows
-export G=/home/flyin1501/ドキュメント
+export G="$HOME"/ドキュメント
 export M=/run/media/flyin1501/Data/mega
 export P=/run/media/flyin1501/Data/mega/Projects
-source /home/flyin1501/.bookmarks
+source "$HOME"/.bookmarks
 
 # for XDG_* directory quick access
-export XDG_DESKTOP_DIR=/home/flyin1501/デスクトップ/
-export XDG_DOWNLOAD_DIR=/home/flyin1501/ダウンロード/
-export XDG_TEMPLATES_DIR=/home/flyin1501/テンプレート/
-export XDG_PUBLICSHARE_DIR=/home/flyin1501/公開/
-export XDG_DOCUMENTS_DIR=/home/flyin1501/ドキュメント/
-export XDG_MUSIC_DIR=/home/flyin1501/音楽/
-export XDG_PICTURES_DIR=/home/flyin1501/画像/
-export XDG_VIDEOS_DIR=/home/flyin1501/ビデオ/
+export XDG_DESKTOP_DIR="$HOME"/デスクトップ/
+export XDG_DOWNLOAD_DIR="$HOME"/ダウンロード/
+export XDG_TEMPLATES_DIR="$HOME"/テンプレート/
+export XDG_PUBLICSHARE_DIR="$HOME"/公開/
+export XDG_DOCUMENTS_DIR="$HOME"/ドキュメント/
+export XDG_MUSIC_DIR="$HOME"/音楽/
+export XDG_PICTURES_DIR="$HOME"/画像/
+export XDG_VIDEOS_DIR="$HOME"/ビデオ/
 
 # vsync off by default
 export vblank_mode=0
