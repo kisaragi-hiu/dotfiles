a5e547c5e436ea7f01c7bfec7f8dcc8e076616a4
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-12-03 00:26:19 +0900

    server-here: accept second argument as path to start server in

--- fish/.config/fish/functions/server-here.fish
+++ fish/.config/fish/functions/server-here.fish
@@ -1,6 +1,13 @@
 function server-here
-    if test -n $argv
-        python -m http.server $argv
+    # server-here port [path]
+    if test -n $argv[1]
+        if test -n $argv[2]
+            pushd $argv[2]
+        end
+        python -m http.server $argv[1]
+        if test -n $argv[2]
+            popd
+        end
     else
         python -m http.server 4000
     end
