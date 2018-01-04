37bb9443520e045a744e917a2269162e93b8f3cb
Kisaragi Hiu <flyingfeather1501@gmail.com>
2018-01-05 00:37:43 +0900

    Add comment to fish_greeting

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,4 +1,6 @@
 function fish_greeting
+    # fish_disable_greeting is 1 or Sudocabulary can't be run
+    # -> don't do anything
     if test $fish_disable_greeting -eq 1 2> /dev/null
         return
     else if not type -q Sudocabulary
