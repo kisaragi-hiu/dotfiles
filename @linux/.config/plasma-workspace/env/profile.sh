f2cc2f4acbcec4651daa9de244209cfae0f04672
Kisaragi Hiu <flyingfeather1501@gmail.com>
2018-01-07 16:21:30 +0900

    Profile: Continue restructure and add $DOTFILES_DIR

--- @linux/.config/plasma-workspace/env/profile.sh
+++ @linux/.config/plasma-workspace/env/profile.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 # shellcheck disable=SC1090
 
+# == directory shortcuts ==
+export D=/run/media/flyin1501/Data
+export C=/run/media/flyin1501/Windows
+export G="$HOME"/ドキュメント
+export M=/run/media/flyin1501/Data/mega
+export P=/run/media/flyin1501/Data/mega/Projects
+source "$HOME"/.bookmarks
+
+# for XDG_* directory quick access
+export XDG_DESKTOP_DIR="$HOME"/デスクトップ/
+export XDG_DOWNLOAD_DIR="$HOME"/ダウンロード/
+export XDG_TEMPLATES_DIR="$HOME"/テンプレート/
+export XDG_PUBLICSHARE_DIR="$HOME"/公開/
+export XDG_DOCUMENTS_DIR="$HOME"/ドキュメント/
+export XDG_MUSIC_DIR="$HOME"/音楽/
+export XDG_PICTURES_DIR="$HOME"/画像/
+export XDG_VIDEOS_DIR="$HOME"/ビデオ/
+
+export DOTFILES_DIR="$HOME"/.dotfiles/
+
 # == UI ==
 # use qt5ct (within plasma as well)
 export QT_QPA_PLATFORMTHEME=qt5ct
@@ -36,39 +56,17 @@ export PATH=\
 "$HOME"/.gem/ruby/2.4.0/bin:\
 $PATH
 
-# for storing ssh keys with kdewallet
-export SSH_ASKPASS="/usr/bin/ksshaskpass"
+# == app behavior controls ==
+export SSH_ASKPASS="/usr/bin/ksshaskpass" # for storing ssh keys with kdewallet
+export OCIO="$DOTFILES_DIR"/_filmic-blender/config.ocio # filmic blender
+export WINEDEBUG=-all # turn off wine debugging
+
+# editor
+export VISUAL=nvim
+export EDITOR=nvim
 
 # what was this here for??
 # export fish_alias="$HOME/.config/fish/conf.d/alias.fish"
 # export fish_alias_linux="$HOME/.config/fish/conf.d/alias-linux.fish"
-
-# filmic Blender can be loaded this way
-export OCIO="$HOME"/.dotfiles/_filmic-blender/config.ocio
-
-# turn off wine debugging
-export WINEDEBUG=-all
-
-# vim
-export VISUAL=nvim
-export EDITOR=nvim
-
-# == directory shortcuts ==
-export D=/run/media/flyin1501/Data
-export C=/run/media/flyin1501/Windows
-export G="$HOME"/ドキュメント
-export M=/run/media/flyin1501/Data/mega
-export P=/run/media/flyin1501/Data/mega/Projects
-source "$HOME"/.bookmarks
-
-# for XDG_* directory quick access
-export XDG_DESKTOP_DIR="$HOME"/デスクトップ/
-export XDG_DOWNLOAD_DIR="$HOME"/ダウンロード/
-export XDG_TEMPLATES_DIR="$HOME"/テンプレート/
-export XDG_PUBLICSHARE_DIR="$HOME"/公開/
-export XDG_DOCUMENTS_DIR="$HOME"/ドキュメント/
-export XDG_MUSIC_DIR="$HOME"/音楽/
-export XDG_PICTURES_DIR="$HOME"/画像/
-export XDG_VIDEOS_DIR="$HOME"/ビデオ/
 
 source "$HOME"/.private_env
