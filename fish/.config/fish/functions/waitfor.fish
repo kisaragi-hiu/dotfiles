bd6c563ff1437bc997f2462b2acb9a102cd34b7e
Kisaragi Hiu <flyingfeather1501@gmail.com>
2018-01-10 23:21:07 +0900

    waitfor: don't take a command to eval, just return after the signal

--- fish/.config/fish/functions/waitfor.fish
+++ fish/.config/fish/functions/waitfor.fish
@@ -1,11 +1,10 @@
 function waitfor
-    # Wait for $argv[1] to come into existance, then run $argv[2..-1]
+    # Block until $argv[1] to come into existance
     while true
         sleep 1
         if test -e $argv[1]
             break
         end
     end
-    and eval $argv[2..-1]
 end
 
