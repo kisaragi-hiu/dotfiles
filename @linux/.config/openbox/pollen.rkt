90facd22a547d7bf8e85e73a507e370c31fde7f8
Kisaragi Hiu <flyingfeather1501@gmail.com>
2018-02-11 18:41:31 +0900

    openbox: pollen.rkt: add def/xexpr-switch and execute, use them for actions etc.
    like define/txexpr in kisaragi-hiu.com

--- @linux/.config/openbox/pollen.rkt
+++ @linux/.config/openbox/pollen.rkt
@@ -4,6 +4,17 @@
 
 (provide (all-defined-out))
 
+;; I want a contracted version of this, but simply passing the contract breaks
+;; as a new input is added
+(define-syntax (def/xexpr-switch stx)
+  (syntax-case stx ()
+    [(_ (name args ... . rest) body ...)
+     #'(begin
+         (define (name #:return-xexpr? [return-xexpr? #f] args ... . rest)
+           ;; this is inconsistent with normal define as now all bodies are wrapped in one
+           ;; hence the inconsistent name def/xexpr-switch
+           ((if return-xexpr? identity xexpr->string*) body ...)))]))
+
 (define/contract (xexpr->string* . xexprs)
   ;; xexpr->string only takes one xml tree. Work around that, take multiple xexprs and turn them into a string.
   (->* () () #:rest (listof xexpr?) string?)
@@ -12,39 +23,37 @@
       (string-replace _ #rx"^<root>" "")
       (string-replace _ #rx"</root>$" "")))
 
-(define (action/undecorate-and-maximize)
-  (xexpr->string* '(action ([name "Undecorate"]))
-                  '(action ([name "Maximize"]))))
-(define (action/decorate-and-unmaximize)
-  (xexpr->string* '(action ([name "Decorate"]))
-                  '(action ([name "Unmaximize"]))))
-(define (action/toggle-maximize-and-decorations)
-  (xexpr->string `(action ([name "If"])
-                          (maximized "yes")
-                          (then (action ([name "Decorate"]))
-                                (action ([name "Unmaximize"])))
-                          (else (action ([name "Undecorate"]))
-                                (action ([name "Maximize"]))))))
+(def/xexpr-switch (action/undecorate-and-maximize)
+  '(action ([name "Undecorate"]))
+  '(action ([name "Maximize"])))
+(def/xexpr-switch (action/decorate-and-unmaximize)
+  '(action ([name "Decorate"]))
+  '(action ([name "Unmaximize"])))
+(def/xexpr-switch (action/toggle-maximize-and-decorations)
+  `(action ([name "If"])
+           (maximized "yes")
+           (then (action ([name "Decorate"]))
+                 (action ([name "Unmaximize"])))
+           (else (action ([name "Undecorate"]))
+                 (action ([name "Maximize"])))))
 
-(define/contract (action/notify summary [body " "] #:icon [icon #f])
-  (->* (string?) (string? #:icon (or/c #f string?)) string?)
-  (define command (string-join `("notify-send"
-                                 ,(string-append "\""summary"\"")
-                                 ,(string-append "\""body"\"")
-                                 ,(if icon
-                                      (string-append "--icon " "\""icon"\"")
-                                      ""))))
-  (xexpr->string
-   `(action ([name "Execute"])
-            (command ,command))))
+(def/xexpr-switch (action/notify summary [body " "] #:icon [icon #f])
+  (let ([icon-arg1 (if icon "--icon" "")] [icon-arg2 (if icon (string-append "\"" icon "\"") "")])
+    (execute #:return-xexpr? #t
+             "notify-send"
+             (string-append "\"" summary "\"")
+             (string-append "\"" body "\"")
+             icon-arg1
+             icon-arg2)))
 
-(define (action/plasma-osd text #:icon icon) ; plasma's osd requires an icon
+(def/xexpr-switch (action/plasma-osd text #:icon icon) ; plasma's osd requires an icon
   `(action ([name "Execute"])
            (command (string-append "qdbus org.kde.plasmashell /org/kde/osdService org.kde.osdService.showText "
                                    ,icon
                                    " "
                                    ,text))))
 
+;; this is only used in one section, no need to use xexpr switch (probably)
 (define/contract (font place name [size 11]
                        #:weight [weight "normal"]
                        #:slant [slant "normal"])
@@ -60,3 +69,6 @@
                 "<font place=\"ActiveWindow\"><name>Roboto</name><size>11</size><weight>normal</weight><slant>normal</slant></font>")
   (check-equal? (action/notify "hello" #:icon "retry")
                 "<action name=\"Execute\"><command>notify-send \"hello\" \" \" --icon \"retry\"</command></action>"))
+
+(def/xexpr-switch (execute . arguments)
+  `(action ([name "Execute"]) (command ,(string-join (map (λ (x) (string-replace x " " "\\ ")) (flatten arguments))))))
