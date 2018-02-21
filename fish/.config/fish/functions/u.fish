4338c89de32ed30115816b03cba28ffb43511873
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-02-21 21:52:30 +0900

    fish: u: add comments

--- fish/.config/fish/functions/u.fish
+++ fish/.config/fish/functions/u.fish
@@ -1,8 +1,12 @@
 function u
+    # TODO overload cd to have : stand for gitroot instead
+    # so `cd :` or `cd :/repo/rel/path` can be used
+    # `cd ./:` should still go to the folder ":"
+
     # cd to git root
     # ported from http://blog.sushi.money/entry/20100211/1265879271
-    cd ./(git rev-parse --show-cdup)
-    if test -n "$argv[1]"
+    cd ./(git rev-parse --show-cdup) # go to git repo root
+    if test -n "$argv[1]" # $1 is relative to git repo
         cd $argv[1]
     end
 end
