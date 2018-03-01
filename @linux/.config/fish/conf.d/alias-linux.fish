9e9424a7ff3b1a5ce9d2ecdb57efc1716d25d5c1
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-03-01 19:29:25 +0900

    @linux: fish: alias ls to just exa without extra message

--- @linux/.config/fish/conf.d/alias-linux.fish
+++ @linux/.config/fish/conf.d/alias-linux.fish
@@ -6,6 +6,6 @@ alias screenshot 'import -window root'
 alias pacman-list-orphans 'pacman -Qtd'
 alias pacman-remove-orphans 'sudo pacman -Rns (pacman -Qtdq)'
 
-alias ls 'echo Using exa.; echo; exa'
+alias ls 'exa'
 
 alias racket-slideshow '/usr/bin/slideshow'
