07db2bc809b83efaabef61a1a2e43aa53f67c2a1
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-05-21 22:27:38 +0800

    emacs: increase gc threshold during init
    from https://github.com/purcell/emacs.d

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -4,6 +4,13 @@
 ;;; Code:
 (add-to-list 'load-path
              (expand-file-name "kisaragi/" user-emacs-directory))
+
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'after-init-hook
+           (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
 (load "kisaragi-configure-pm.el")
 
 (load "kisaragi-packages.el")
