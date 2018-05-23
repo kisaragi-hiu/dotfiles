7f1ba882886f423ec95019d78d8f5bf4bfbfa83d
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-05-24 00:55:08 +0900

    emacs: move each leader-prefix into its own definition; no general-create-definer

--- emacs/.emacs.d/kisaragi/kisaragi-keybinds.el
+++ emacs/.emacs.d/kisaragi/kisaragi-keybinds.el
@@ -3,8 +3,9 @@
 ;;; All the keybindings, in one file.
 ;;; I'm trying to utilize general.el.
 ;;; Code:
+(defconst kisaragi/primary-leader "SPC" "My primary leader key.")
+
 (general-define-key
- "C-," 'parinfer-toggle-mode
  "C-=" 'evil-numbers/inc-at-pt
  "C--" 'evil-numbers/dec-at-pt
  "C-h f" 'helpful-callable
@@ -20,29 +21,46 @@
  "C-x v f" 'vimish-fold
  "C-x v v" 'vimish-fold-delete)
 
-(defconst kisaragi/primary-leader "SPC" "My primary leader key.")
-(general-create-definer kisaragi/primary-leader-def
-  :prefix kisaragi/primary-leader
-  :states 'normal)
-
-(kisaragi/primary-leader-def
+(general-define-key
+ :prefix kisaragi/primary-leader
+ :states 'normal
+ kisaragi/primary-leader 'execute-extended-command
+ "x" 'execute-extended-command
  "b" 'ivy-switch-buffer
- "ff" 'find-file
- "fp" 'find-file-at-point
  "h" 'evil-prev-buffer
- "l" 'evil-next-buffer
- "gs" 'magit-status
- "g↓" 'magit-pull
- "g↑" 'magit-push
- "p" 'parinfer-toggle-mode
- "SPC" 'execute-extended-command
- "x" 'execute-extended-command)
+ "l" 'evil-next-buffer)
 
-(kisaragi/primary-leader-def
+(general-define-key
+ :prefix (key-description (list kisaragi/primary-leader "g"))
+ :states 'normal
+ "" '(:ignore t :wk "magit")
+ "s" 'magit-status
+ "↓" 'magit-pull
+ "↑" 'magit-push)
+
+(general-define-key
+ :prefix (key-description (list kisaragi/primary-leader "f"))
+ :states 'normal
+ "" '(:ignore t :which-key "file")
+ "f" 'find-file
+ "p" 'find-file-at-point
+ "e" '(:ignore t :which-key "edit config")
+ "ed" `(,(lambda () (interactive) (find-file (f-join user-emacs-directory "init.el")))
+        :which-key "init.el")
+ "es" `(,(lambda () (interactive) (find-file (f-join user-emacs-directory "kisaragi" "kisaragi-settings.el")))
+        :which-key "settings.el")
+ "ep" `(,(lambda () (interactive) (find-file (f-join user-emacs-directory "kisaragi" "kisaragi-packages.el")))
+        :which-key "packages.el")
+ "ek" `(,(lambda () (interactive) (find-file (f-join user-emacs-directory "kisaragi" "kisaragi-keybinds.el")))
+         :which-key "keybinds.el"))
+
+(general-define-key
+ :prefix kisaragi/primary-leader
+ :states 'normal
  :keymaps 'org-mode-map
  "c" 'org-toggle-checkbox)
 
-(kisaragi/primary-leader-def
+(general-define-key
  :keymaps '(xah-elisp-mode-map emacs-lisp-mode-map)
  "eb" 'eval-buffer
  "ed" 'eval-defun
